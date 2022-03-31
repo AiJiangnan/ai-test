45772f4a030f8e2a8ce06e6b72c6e6a4a3814518
AiJiangnan <jnai@gaojihealth.com>
2022-03-31 20:03:03 +0800

    添加Newman依赖

--- routes/users.ts
+++ routes/users.ts
@@ -1,8 +1,22 @@
-import express from 'express';
+import express, { response } from 'express';
+import newman from 'newman';
+import { Collection } from 'postman-collection';
+import * as test from '../test_collection.json';
+
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req: any, res: any, next: (v: any) => void) {
+router.get('/', (req: any, res: any, next: (v: any) => void) => {
+  var collection = new Collection(test);
+  newman.run({ collection },
+    (err) => {
+      if (err) { throw err; }
+      console.log('collection run complete!');
+    }).on('request', (err, summary) => {
+      console.log(response.header)
+      console.log('\n')
+      console.log(summary.response.stream.toString())
+      console.log('\n')
+    })
   res.send('respond with a resource');
 });
 
